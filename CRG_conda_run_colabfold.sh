4646d68d5a56f5e39043d5a6202535bc49154416
Ni-Ar <arecco.niccolo@gmail.com>
2023-02-27 10:54:41 +0100

    Simplified a lot the job submission script.
    Input and output are specified in the script itself and not with flags.

--- CRG_conda_run_colabfold.sh
+++ CRG_conda_run_colabfold.sh
@@ -2,163 +2,30 @@
 
 # ColabFold script with a conda env.
 # Author: Niccolo' Arecco
-# Last modifications 28/Sept/2022
+# Last modifications 27/Feb/2023
 
-usage() {
-        echo ""
-        echo "Usage: $0 -i <DIR|FASTA> -q <'gpu'|'gpu_long'> <EXTRA OPTIONS>"
-        echo -e "\nRequired Parameters:"
-        echo "-i <DIR|FASTA>          Path to directory with input fasta/a3m files or a csv/tsv file or a fasta file or an a3m file."
-        echo "-q <'gpu'|'gpu_long'>   Name of the CRG gpu queue to use. This script selects the right hard resource to use for the prediction based on the queue."
-        echo -e "\nAdditional Parameters:"
-        echo "-o <OUTPUT/PATH/DIR>    Relative path from ${HOME} to a directory that will store the results. Example: -o sharing/cf_for_friend"
-        echo "-m <'multi'>            Run CF with a specific AF2 multimer mode. Deafult is <'auto'>."
-        echo "-h                      Show this help and exit."
-        echo ""
-        exit 1
-}
+# -- SGE JOB SUBMISSION SETTINGS
+#$ -q gpu,gpu_long
+#$ -pe smp 6
+#$ -V
+#$ -l virtual_free=64G
+#$ -l h_vmem=64G 
+#$ -l h_rt=85:30:50
+#$ -l s_rt=85:30:50
+#$ -terse
+#$ -l gpu=1
+#$ -N colabfold
+#$ -wd /users/mirimia/narecco/software/colabfold
+#$ -o /users/mirimia/narecco/software/colabfold/cf_stdout.log
+#$ -e /users/mirimia/narecco/software/colabfold/cf_stderr.log
+#$ -b y
+# -- the commands to be executed (programs to be run) :
 
-while getopts ":hi:o:q:m:" option; do
-        case $option in
-        h)
-                usage
-                exit 1
-        ;;
-        i)
-                INPUT=$OPTARG
-        ;;
-        o)
-                OUTPUT_DIR=$OPTARG
-        ;;
-        q)
-                QUEUE_NAME=$OPTARG
-        ;;
-        m)     
-                MODEL_TYPE=$OPTARG
-        ;;
-        \?)     
-                echo "Error: '-$OPTARG' is an invalid option"
-                exit
-        ;;
-        esac
-done
-
-# REQUIRED PARAMS
-# Parse params and set defaults
-if [[ "$INPUT" == "" ]] ; then
-    echo -e "\nSpecify the input directory!"
-    echo "-i <DIR|FASTA> Path to directory with input fasta/a3m files or a csv/tsv file or a fasta file or an a3m file."
-    exit
-fi
-
-if [[ "$QUEUE_NAME" == "" ]] ; then
-    echo -e "\nSpecify the CRG queue name.\n\tInput provided: '$QUEUE_NAME'. Select one of:\n\tgpu: 6 hours max, unlimited GPUs (max 10)\n\tgpu_long: 7 days max, max 2 GPUs.\n"
-    echo "-q <gpu|gpu_long> Name of the CRG gpu queue to use. This script selects the right hard resource to use for the prediction based on the queue."
-    exit
-fi
-
-# Print info about the fasta file:
-FASTA_SUFFIX_REGEX="fasta$"
-if [[ $INPUT =~ $FASTA_SUFFIX_REGEX ]]; then
-    # in case the input is a file file that ends with ".fasta" file format
-    INPUT_NAME=$( basename $INPUT .fasta )
-else 
-    # in case the input is a folder
-    INPUT_NAME=$( basename $INPUT )
-fi 
-echo -e "Input name: $INPUT_NAME"
-
-# ADDITIONAL PARAMS
-## OUTPUT
-if [[ "$OUTPUT_DIR" == "" ]] ; then
-
-    # If output is not specified create one based on the user.
-    if [[ $USER =~ "narecco" ]] ; then
-        OUTPUT_DIR="${HOME}/projects/12_Predicted_Structures/data/pdb/CF/${INPUT_NAME}"
-    else
-	    OUTPUT_DIR="${HOME}/pdb/CF/${INPUT_NAME}" 
-    fi
-else 
-    OUTPUT_DIR="${HOME}/$OUTPUT_DIR" 
-    # echo -e "Output directory: $OUTPUT_DIR"     
-fi
-
-# If output dir doesn't exist create one
-if [ -d ${OUTPUT_DIR} ]  ; then
-    echo -e "Output directory already exists!\n${OUTPUT_DIR}\nStopping to avoid re-computing or overwriting!" 
-    exit
-else
-    mkdir -p ${OUTPUT_DIR}
-    # echo -e "Creating output directory for you."
-fi
-
-## MULTIMER MODEL 
-if [[ "$MODEL_TYPE" == "multi" ]] ; then
-    MODEL_CMD=$( echo "--model-type AlphaFold2-multimer-2" )
-elif [[ "$MODEL_TYPE" == "" ]] ; then
-    MODEL_CMD=$( echo "--model-type auto" )
-fi
-
-# Time ref
-DATE=`date +%Y_%m_%d`
-
-# How many resources to use?
-Num_Ram=64      # In GigaBytes for the CPUs
-Num_Processes=6 # For the CPUs
-
-# Based on CRG max allowed resources
-if [[ "$QUEUE_NAME" == "gpu" ]] ; then
-   Num_Hours=01
-   Num_GPUs=1
-elif [ "$QUEUE_NAME" == "gpu_long" ] ; then
-   Num_Hours=167
-   Num_GPUs=1
-else 
-   echo -e "QUEUE_NAME must be either 'gpu' or 'gpu_long', not ${$QUEUE_NAME}\nAborted\n"
-   exit 
-fi 
-
-# Set the working directory where the colabfold params are saved.
-CF_DIR="${HOME}/software/colabfold"
-cd $CF_DIR
-
-## Exporting local variables 
-# export NVIDIA_VISIBLE_DEVICES='all'
-
-## TensorFlow control
-# "1" to enable unified memory
-export TF_FORCE_UNIFIED_MEMORY="1" 
-
-## JAX control
-# If your JAX process fails with out-of-memory (OOM) errors, the following environment variables can be used to override the default behavior:
-
-# Disables the preallocation behavior, potentially decreasing the overall memory usage. This behavior is more prone to GPU memory fragmentation, meaning a JAX program that uses most of the available GPU memory may OOM with preallocation disabled
-# export XLA_PYTHON_CLIENT_PREALLOCATE=false
-
-# If jobs fails try: 4.0, 2.0, 9.0, 0.5
-# If preallocation is enabled, this makes JAX preallocate XX% of currently-available GPU memory, instead of the default 90%. Lowering the amount preallocated can fix Out-Of-Memories errors that occur when the JAX program starts
-export XLA_PYTHON_CLIENT_MEM_FRACTION="4.0"
-
-## Other stuff
-# Usually required otherwise I get UNKNOWN error. This slows down compiling the model. Future CUDA drivers might not need this anymore.
-export XLA_FLAGS="--xla_gpu_force_compilation_parallelism=1" 
-# export XLA_PYTHON_CLIENT_ALLOCATOR="platform"
-# export TF_FORCE_GPU_ALLOW_GROWTH="true"
-
-## ColabFold control
-export COLABFOLDDIR="${HOME}/software/colabfold"
-export XDG_CACHE_HOME="${HOME}/software/colabfold"
-
-## Set up directories paths
-JOBS_OUT_DIR="${HOME}/qsub_out/${DATE}/CF"
-mkdir -p ${JOBS_OUT_DIR}
-
-# Print info before running the job
-echo -e "\nqsub params:\n\tCRG queue name: ${QUEUE_NAME}\n\tMax wallclock time allowed: ${Num_Hours}:59 (hh:mm)\n\tNum Processes: ${Num_Processes}\n\tCPU Ram per process: ${Num_Ram}Gb\n\tNum GPU(s): ${Num_GPUs} (NVIDIA RTX 2080 Ti)\n\tOuput: ${OUTPUT_DIR}\n\tLog file: ${JOBS_OUT_DIR}/${INPUT_NAME}_std{out|err}.log"
-
-echo -e "\nGPU exported variables:\n\tVisible GPUs: $NVIDIA_VISIBLE_DEVICES\n\tTensorFlow unified memory: $TF_FORCE_UNIFIED_MEMORY\n\tAllow GPU memory pre-allocation: $XLA_PYTHON_CLIENT_PREALLOCATE\n\tPre-allocated percentage of currently-available GPU memory (if allowed): $XLA_PYTHON_CLIENT_MEM_FRACTION%\n\tMinimal GPU footprint: $XLA_PYTHON_CLIENT_ALLOCATOR\n\tAllow GPU growth: $TF_FORCE_GPU_ALLOW_GROWTH"
-
-sleep 2
+# TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
+# echo -e "\n$TIMESTAMP start test\n"
+# python python_configuration_test.py  
+# TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
+# echo -e "\n$TIMESTAMP end test\n"
 
 # Run Colabfold with required parameters using qsub
 # parameters details:
@@ -166,14 +33,16 @@ sleep 2
 # --templates: Use templates from pdb. PDB structures from the database enter the MSA module but are less important compared to the MSA itself. 
 # --num-recycle N: AF parameter to perform N recyling step on the predicted model i.e. from the xyz PDB go back to the MSA module and perform the structure model again
 
-qsub -q ${QUEUE_NAME} -V -pe smp ${Num_Processes} -l gpu=${Num_GPUs} \
-     -l virtual_free=${Num_Ram}G,h_vmem=${Num_Ram}G,h_rt=${Num_Hours}:59:00 \
-     -terse -wd ${CF_DIR} -N cf-${INPUT_NAME} \
-     -o "${JOBS_OUT_DIR}/${INPUT_NAME}_stdout.log" \
-     -e "${JOBS_OUT_DIR}/${INPUT_NAME}_stderr.log" \
-     -b y \
-     colabfold_batch --amber --templates --num-recycle 4 --use-gpu-relax --num-models 5 --model-order 1,2,3,4,5 \
-                     --random-seed 16 ${MODEL_CMD} ${INPUT} ${OUTPUT_DIR}
+PROJ_NAME="12_Predicted_Structures"
+IN_FASTA_NAME="PRC2_SUZ12_ex4_inclu_cb-format.fasta"
+INPUT="/users/mirimia/narecco/projects/${PROJ_NAME}/data/fasta/${IN_FASTA_NAME}"
+OUTPUT="/users/mirimia/narecco/projects/${PROJ_NAME}/data/pdb/CF/PRC2_ex4_exclu"
+
+# load NVIDIA driver's CUDA version 12+
+module load CUDA/12.0.0
+colabfold_batch --amber --templates --num-recycle 20 --recycle-early-stop-tolerance 0.5 \
+                --use-gpu-relax --num-models 5 --model-order 1,2,3,4,5 \
+                --random-seed 16 --model-type auto ${INPUT} ${OUTPUT}
 
 # no vaya a ser que == por si (que)
 # Coge el paraguas, por si llueve # indicativo
